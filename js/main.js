00c78b9b53e39915ce4209f05914575e3da02cbe
JABedford <james.bedford1991@gmail.com>
2018-07-17 09:43:09 +0100

    initial commit
    mass upload commence

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/**
+ * Register the service worker
+ */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./service-worker.js', {
+      scope: './'
+    })
+    .then(function () {
+      console.log('Service Worker registered');
+    })
+    .catch(function () {
+      console.log('Service Worker failed to register');
+    })
+}
+
 let restaurants,
   neighborhoods,
   cuisines
@@ -141,9 +157,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -151,12 +168,12 @@ createRestaurantHTML = (restaurant) => {
   neighborhood.innerHTML = restaurant.neighborhood;
   li.append(neighborhood);
 
-  const address = document.createElement('p');
+  const address = document.createElement('address');
   address.innerHTML = restaurant.address;
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details of ' + restaurant.name;
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
@@ -176,3 +193,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+/**
+ * Add title to the iframe element
+ */
+window.addEventListener('load', function () {
+  let iframeElt = document.querySelector('iframe');
+  iframeElt.setAttribute('title', 'iframe map');
+  iframeElt.setAttribute('tabindex', '-1');
+});
